450811bf189f4ca22419cc8f72fc082293347a27
Filipe Pires <liipepires@gmail.com>
2018-11-29 17:28:02 -0300

    add join path no webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
+    // entry: "./src/index.js",
+    entry: path.join(__dirname, "src", "index.js"),
     output: {
         path: path.join(__dirname, "build"),
         filename: "bundle.js"
@@ -31,17 +32,18 @@ module.exports = {
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
         new HtmlWebpackPlugin({
-            template: "build/index.html",
-            filename: "./index.html"
+            template: path.join(__dirname, "build", "index.html")
+            // template: "build/index.html",
+            // filename: "./index.html"
         }),
         new CopyWebpackPlugin([
             // Copy directory contents to {output}/to/directory/
-            { from: path.join(__dirname, "src/assets") }
+            { from: path.join(__dirname, "src", "assets") }
         ])
     ],
     devServer: {
         contentBase: "./build",
         hot: true
     },
-    devtool: 'source-map'
+    devtool: "source-map"
 };
